5fed42b647c54be4c55f5bb1698b36a5eacc1b8a
Jadiel Santos <jadielsilva11406@gmail.com>
2023-11-22 15:58:09 -0300

    feat: finalizado o desafio(natty)

---
+++ natty/script.js
@@ -0,0 +1,18 @@
+// lista inicial
+let lista = [0,1]
+
+// Criar um loop para imprimir os primeiros 40 numeros da sequência de fibonacci
+function sequenciaDeFibonacci() {
+    for(n=1;n<=38;n++) {
+
+        // Somando o item anterior ao atual
+        const soma = lista[n - 1] + lista[n]
+
+        // Atualizando a lista com os números referente a sequência de fibonacci
+        lista.push(soma)
+    }
+
+    return lista
+}
+
+console.log(`Sequencia de fibonacci: ${sequenciaDeFibonacci()}`)
